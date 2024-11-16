983c0d43d0d2542456746b8e64e30e2259f44c56
Beammi1 <beam7299.com@gmail.com>
2024-11-16 18:00:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 'use client';
+
 import { useState } from "react";
 
 export default function Home() {
   return (
-    
+    <div> 
+      <span>
+        <h1 className="text-bold">Aldthawim</h1>
+        <ul>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+        </ul>
+      </span>
+    </div>
   );
 }
